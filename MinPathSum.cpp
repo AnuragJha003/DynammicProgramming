250b7d0f57f0d0707a4f02869fbdeb8c969f1a1d
AnuragJha003 <97457228+AnuragJha003@users.noreply.github.com>
2023-04-07 19:07:44 +0530

    Create MinPathSum.cpp

---
+++ MinPathSum.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+int helper(int i, int j,vector<vector<int>> &grid,vector<vector<int>> &dp)
+{
+     if(i==0 && j == 0)
+       return grid[0][0];
+
+    if(i<0 || j<0)
+      return 1e9;
+
+    if(dp[i][j]!=-1) 
+        return dp[i][j];
+    
+  int up = grid[i][j]+helper(i-1,j,grid,dp);
+  int left = grid[i][j]+helper(i,j-1,grid,dp);
+  
+  return dp[i][j] = min(up,left);
+  
+}
+
+    int minPathSum(vector<vector<int>>& matrix) {
+
+                                   
+        int n = matrix.size();
+        int m = matrix[0].size();
+       // vector<vector<int> > dp(n,vector<int>(m,-1));
+        //return helper(n-1,m-1,grid,dp);
+       vector<vector<int> > dp(n,vector<int>(m,0));
+    for(int i=0; i<n ; i++){
+        for(int j=0; j<m; j++){
+            if(i==0 && j==0) dp[i][j] = matrix[i][j];
+            else{
+                
+                int up = matrix[i][j];
+                if(i>0) up += dp[i-1][j];
+                else up += 1e9;
+                
+                int left = matrix[i][j];
+                if(j>0) left+=dp[i][j-1];
+                else left += 1e9;
+                
+                dp[i][j] = min(up,left);
+            }
+        }
+    }
+    
+    return dp[n-1][m-1];
+    }
+};
+https://leetcode.com/problems/minimum-path-sum/
